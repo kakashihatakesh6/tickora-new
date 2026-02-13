d6b936ba45b1399d7b6254ce721c8daca57e3d86
Nikhil <nikhil@gpu.net>
2026-02-13 19:32:23 +0530

    feat: updated .env

--- next.config.ts
+++ next.config.ts
@@ -65,7 +65,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/v1/:path*',
-        destination: 'http://localhost:8080/api/v1/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080'}/api/v1/:path*`,
       },
     ];
   },
